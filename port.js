89e6cce1dfc90226fddac9284ddcef2537de76df
vaibhavvyas <84579873+vaibhavvyas@users.noreply.github.com>
2023-12-20 21:58:12 +0530

    Add files via upload

---
+++ port.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // You can add logic to handle the form submission here
+    alert('Form submitted!');
+});
